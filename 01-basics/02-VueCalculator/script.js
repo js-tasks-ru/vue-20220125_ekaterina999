7fcc7090a5c411fcd474ce0a2d90f38d61da8761
Ekaterina Voropaeva <ekaterina.voropaeva@t-systems.com>
2022-01-29 22:47:08 +0300

    VueCalculator was added

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,36 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      firstNum: 0,
+      secondNum: 0,
+      operation: '',
+    };
+  },
+
+  computed: {
+    countSum() {
+      let res = 0;
+
+      if (this.operation === 'sum') {
+        res = this.firstNum + this.secondNum;
+      }
+
+      if (this.operation === 'subtract') {
+        res = this.firstNum - this.secondNum;
+      }
+
+      if (this.operation === 'multiply') {
+        res = this.firstNum * this.secondNum;
+      }
+
+      if (this.operation === 'divide') {
+        res = this.firstNum / this.secondNum;
+      }
+
+      return res;
+    },
+  },
+}).mount('#app');
