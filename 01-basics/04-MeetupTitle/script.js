9bfbfb25c2c224f858a10302fb8e680950cf2d31
Ekaterina Voropaeva <ekaterina.voropaeva@t-systems.com>
2022-02-03 18:44:04 +0300

    added MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,20 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const app = createApp({
+  data() {
+    return {
+      choosenValue: '',
+      title: '',
+    };
+  },
+
+  watch: {
+    choosenValue: {
+      deep: true,
+      handler(newVal, oldVal) {
+        fetchMeetupById(newVal).then((val) => (this.title = val.title));
+      },
+    },
+  },
+}).mount('#app');
