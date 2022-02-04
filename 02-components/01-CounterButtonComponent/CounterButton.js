8c00fd704b98b926ab6edf7fd0050273bd3e6306
Ekaterina Voropaeva <ekaterina.voropaeva@t-systems.com>
2022-02-04 12:54:37 +0300

    added counterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,23 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  methods: {
+    increment() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="increment">{{ count }}</button>`,
 });
