39aacc41b08c39970bdb396f7559409c6b1210ec
Ekaterina Voropaeva <ekaterina.voropaeva@t-systems.com>
2022-02-14 12:01:41 +0300

    fixed

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -2,11 +2,6 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
 import { fetchMeetupById } from './meetupService.js';
-import MeetupDescription from './../02-MeetupDescription/MeetupDescription.js';
-import MeetupCover from './../03-MeetupCover/MeetupCover.js';
-import MeetupInfo from './../04-MeetupInfo/MeetupInfo.js';
-import MeetupAgendaItem from './../05-MeetupAgenda/MeetupAgendaItem.js';
-import MeetupAgenda from './../05-MeetupAgenda/MeetupAgenda.js';
 import MeetupView from './../06-MeetupView/MeetupView.js';
 
 export default defineComponent({
@@ -17,11 +12,6 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
-    MeetupDescription,
-    MeetupCover,
-    MeetupInfo,
-    MeetupAgendaItem,
-    MeetupAgenda,
     MeetupView,
   },
 
@@ -36,6 +26,7 @@ export default defineComponent({
   data() {
     return {
       meetup: null,
+      errorMes: '',
     };
   },
 
@@ -44,12 +35,13 @@ export default defineComponent({
       immediate: true,
       handler(newV) {
         this.meetup = null;
+        this.errorMes = null;
         fetchMeetupById(newV).then(
           (meetup) => {
             this.meetup = meetup;
           },
           (meetup) => {
-            this.meetup = 'error';
+            this.errorMes = meetup.message;
           },
         );
       },
@@ -59,7 +51,7 @@ export default defineComponent({
   template: `
     <div class="page-meetup">
       <!-- meetup view -->
-      <ui-container v-if="meetup === 'error'">
+      <ui-container v-if="errorMes">
         <ui-alert>Test Error</ui-alert>
       </ui-container>
       <ui-container v-else-if="!meetup">
