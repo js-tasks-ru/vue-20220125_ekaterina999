3c23936a9110637a5682195ce9f61901046e573d
Ekaterina Voropaeva <ekaterina.voropaeva@t-systems.com>
2022-02-25 18:52:29 +0300

    added TheToaster

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
+    <template v-for="toast in toasts" :key="toast.message">
+      <div v-if="toast.show && toast.type === 'success'" class="toast toast_success">
+        <ui-icon class="toast__icon" icon="check-circle" />
+        <span>{{ toast.message }}</span>
+      </div>
 
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+      <div v-else-if="toast.show && toast.type === 'error'" class="toast toast_error">
+        <ui-icon class="toast__icon" icon="alert-circle" />
+        <span>{{ toast.message }}</span>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -19,6 +21,41 @@ export default {
   name: 'TheToaster',
 
   components: { UiIcon },
+
+  data() {
+    return {
+      toasts: [],
+      id: 0,
+    };
+  },
+
+  methods: {
+    error(message) {
+      this.toasts.push({
+        id: this.id,
+        message: message,
+        type: 'error',
+        show: true,
+      });
+      setTimeout(this.setTime, 5000, this.id);
+      this.id++;
+    },
+
+    success(message) {
+      this.toasts.push({
+        id: this.id,
+        message: message,
+        type: 'success',
+        show: true,
+      });
+      setTimeout(this.setTime, 5000, this.id);
+      this.id++;
+    },
+
+    setTime(toastId) {
+      this.toasts[toastId].show = false;
+    },
+  },
 };
 </script>
 
