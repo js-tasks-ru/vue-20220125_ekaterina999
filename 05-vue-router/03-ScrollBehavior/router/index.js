7eda8c03c5590f0efe41c4311d959a9fe4376b5a
Voropaeva <ekaterina.voropaeva@telekom.com>
2022-10-20 13:25:19 +0300

    added 05-vue router tasks

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -41,4 +41,18 @@ export const router = createRouter({
       ],
     },
   ],
+
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+      };
+    } else if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+      return {};
+    } else if (from.meta.saveScrollPosition) {
+      return { top: 0, left: 0 };
+    }
+
+    return savedPosition || { top: 0, left: 0 };
+  },
 });
